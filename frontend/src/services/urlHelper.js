b6f86d2acd453127ad6e389a1a19e7b53cc2a03c
Avinash <ak188269@gmail.com>
2024-01-17 17:13:15 +0530

    add url for dev mode on condition

--- frontend/src/services/urlHelper.js
+++ frontend/src/services/urlHelper.js
@@ -3,7 +3,7 @@ const isDevMode = ()=> (process.env.NODE_ENV == 'development');
 const URL = {
     // user apis
     // for localhost   'http://localhost:8080/api/v1/user' :
-    USER_API_BASE_URL : "https://affworld-backend.vercel.app/api/v1/user" ,
+    USER_API_BASE_URL : isDevMode() ? "http://localhost:8080/api/v1/user" : "https://affworld-backend.vercel.app/api/v1/user" ,
     USER_REGISTER_API :  '/register' ,
     USER_LOGIN_API :  '/login' ,
     USER_LOGOUT_API :  '/logout' ,
@@ -12,7 +12,7 @@ const URL = {
 
     //  secret apis
     // for localhost 'http://localhost:8080/api/v1/secret' :
-    SECRET_API_BASE_URL :  "https://affworld-backend.vercel.app/api/v1/secret" ,
+    SECRET_API_BASE_URL : isDevMode() ? 'http://localhost:8080/api/v1/secret' : "https://affworld-backend.vercel.app/api/v1/secret" ,
     ADD_SECRET_API :  '/add',
     EDIT_SECRET_API : '/edit',
     DELETE_SECRET_API : '/delete',
