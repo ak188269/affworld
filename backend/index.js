084d1e24671c8c783d0a9f3aea795b5ff1c92cbc
Avinash <ak188269@gmail.com>
2024-01-17 15:27:09 +0530

    add frontend url for cors error

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ startDatabase(process.env.DB_URL);
 app.use(express.json( { limit: '50mb' } ));
 app.use(express.urlencoded( { extended: true, limit: '10mb' } ));
 app.use(cookieParser());
-app.use(cors({origin : ["http://localhost:3000"],credentials: true}));
+app.use(cors({origin : ["http://localhost:3000",'https://affworld.vercel.app'],credentials: true}));
 
 
 
