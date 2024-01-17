03892c4710c1b6502a3ea84037ceb5793f28ce36
Avinash <ak188269@gmail.com>
2024-01-17 15:25:14 +0530

    add favicon and title

--- frontend/src/services/urlHelper.js
+++ frontend/src/services/urlHelper.js
@@ -2,7 +2,8 @@ const isDevMode = ()=>process.env.NODE_ENV !== 'production';
 
 const URL = {
     // user apis
-    USER_API_BASE_URL : isDevMode ?  'http://localhost:8080/api/v1/user' : "https://affworld-backend.vercel.app" ,
+    // for localhost   'http://localhost:8080/api/v1/user' :
+    USER_API_BASE_URL : "https://affworld-backend.vercel.app/api/v1/user" ,
     USER_REGISTER_API :  '/register' ,
     USER_LOGIN_API :  '/login' ,
     USER_LOGOUT_API :  '/logout' ,
@@ -10,7 +11,8 @@ const URL = {
     SIGN_IN_WITH_GOOGLE_API : "/auth/google",
 
     //  secret apis
-    SECRET_API_BASE_URL :  isDevMode ? 'http://localhost:8080/api/v1/secret' : "https://affworld-backend.vercel.app" ,
+    // for localhost 'http://localhost:8080/api/v1/secret' :
+    SECRET_API_BASE_URL :  "https://affworld-backend.vercel.app/api/v1/secret" ,
     ADD_SECRET_API :  '/add',
     EDIT_SECRET_API : '/edit',
     DELETE_SECRET_API : '/delete',
