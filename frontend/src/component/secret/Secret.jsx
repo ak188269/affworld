565eb4b62eb6822daa0a15b12bc694ce8dd6a7ba
Avinash <ak188269@gmail.com>
2024-01-17 14:45:17 +0530

    add pin on secret

--- frontend/src/component/secret/Secret.jsx
+++ frontend/src/component/secret/Secret.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const Secret = ({secret="Some secret message" , isUser = false}) => {
   return (
    <>
-   <div className='border border-black  rounded-lg overflow-hidden  w-[175px] sm:w-[30%] aspect-square max-w-[230px] flex flex-col'>
-    <h1 className={`p-1 px-2 bg-[#0C2D48] text-white`}> {isUser ? "Your" : "Someone" }  secret </h1>
+   <div className='border border-black  rounded-lg   w-[175px] sm:w-[30%] aspect-square max-w-[230px] flex flex-col relative'>
+    <img src={"/images/pin_blue.png"} alt="." className='w-[35px] absolute -top-3 -left-3'/>
+    <h1 className={`p-1 px-2 bg-[#0C2D48] text-white rounded-tl-lg rounded-tr-lg`}> {isUser ? "Your" : "Someone" }  secret </h1>
     <div className={`p-2  text-ellipsis text-sm ${isUser ? 'text-[red]' : 'text-black'}`}>
       
         {secret}
--- frontend/src/services/urlHelper.js
+++ frontend/src/services/urlHelper.js
@@ -2,7 +2,7 @@ const isDevMode = ()=>process.env.NODE_ENV !== 'production';
 
 const URL = {
     // user apis
-    USER_API_BASE_URL : isDevMode ?  'http://localhost:8080/api/v1/user' : "something" ,
+    USER_API_BASE_URL : isDevMode ?  'http://localhost:8080/api/v1/user' : "https://affworld-backend.vercel.app" ,
     USER_REGISTER_API :  '/register' ,
     USER_LOGIN_API :  '/login' ,
     USER_LOGOUT_API :  '/logout' ,
@@ -10,7 +10,7 @@ const URL = {
     SIGN_IN_WITH_GOOGLE_API : "/auth/google",
 
     //  secret apis
-    SECRET_API_BASE_URL :  isDevMode ? 'http://localhost:8080/api/v1/secret' : "something" ,
+    SECRET_API_BASE_URL :  isDevMode ? 'http://localhost:8080/api/v1/secret' : "https://affworld-backend.vercel.app" ,
     ADD_SECRET_API :  '/add',
     EDIT_SECRET_API : '/edit',
     DELETE_SECRET_API : '/delete',
